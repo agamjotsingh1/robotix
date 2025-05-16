eb741feb7887301e6c426be01413844325f7ec27
agamjotsingh1 <agamjot.s.kohli@gmail.com>
2025-05-16 23:49:39 +0530

    commit

--- summer_tasks/task2/main.py
+++ summer_tasks/task2/main.py
@@ -1,20 +1,26 @@
 import cv2
+import sys
 from ultralytics import YOLO
 
 # Load the YOLOv8 model (you can use 'yolov8n.pt', 'yolov8s.pt', 'yolov8m.pt', etc.)
 model = YOLO("yolov8n.pt")  # nano model for speed
 
-# Open webcam
-cap = cv2.VideoCapture(0)  # change to a file path if you want to use a video file
+# Open webcam or the video (if provided is sysargs)
+if(len(sys.argv) == 1): cap = cv2.VideoCapture(0)
+else: cap = cv2.VideoCapture(sys.argv[1])
 
+# Calculate which Zone the Box Centroid lies in
+# Zone = Left => Centroid is in the first 1/3rd of the camera POV 
+# Zone = Center => Centroid is in the middle 1/3rd of the camera POV 
+# Zone = Right => Centroid is in the end 1/3rd of the camera POV 
 def zone(centroid, width):
     x = centroid[0]
     if x < width/3:
-        return -1
+        return "Left"
     elif width/3 <= x < 2*width/3:
-        return 0
+        return "Center"
     else:
-        return 1
+        return "Right"
 
 while True:
     ret, frame = cap.read()
@@ -37,7 +43,8 @@ while True:
     cv2.line(frame, (x2, 0), (x2, height), (0, 255, 0), 2)  # Green line
     boxes = results[0].boxes
 
-    if(len(boxes) != 1):
+    # Detected more than one object, then stop
+    if len(boxes) != 1:
         print("Stop")
     else:
         x1, y1, x2, y2 = boxes.xyxy[0] # x1, y1, x2, y2
@@ -51,9 +58,7 @@ while True:
             object_zone = zone(centroid, width)
 
             cv2.circle(frame, (int(centroid[0]), int(centroid[1])), radius=5, color=(0, 0, 255), thickness=-1)
-            if object_zone == -1: print("Left")
-            elif object_zone == 0: print("Center")
-            elif object_zone == 1: print("Right")
+            print(object_zone)
 
     annotated_frame = results[0].plot()  # draw predictions on the frame
 
